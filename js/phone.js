f34d3087971cf7f3446e224314834c80483701be
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-07-28 12:24:57 +0600

    write some line of code for set a event handeler for search button to fetching data from server and call a function to basically display the data to print console

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,24 @@
-console.log("phone hunt");
+// function: for loading(fetching) all phone from server.
+const loadingAllPhones = async () => {
+  // fetching data from server
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=iphone`
+  );
+  const data = await response.json();
+  displayPhone(data);
+};
+
+// function: for displaying phone as card on website
+const displayPhone = (phones) => {
+  console.log(phones);
+};
+
+// event: function for trigger a event when clicking on search button
+const searchingPhone = () => {
+  console.log("searching phone...");
+
+  // for unknown delay time
+  setTimeout(() => {
+    loadingAllPhones();
+  }, 3000);
+};
