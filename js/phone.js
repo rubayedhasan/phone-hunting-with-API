2deab26e59303f60b91d5640a7d5d072d2f379a6
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-07-28 13:05:44 +0600

    add a loading spinner section after search-box section. and write some line of code to showing it before fetcing data and hide it after fetching data

--- js/phone.js
+++ js/phone.js
@@ -1,21 +1,38 @@
+/**
+ * getting HTML elements
+ *
+ * */
+// element:: loading spinner
+const loader = document.querySelector("#loading-spinner");
+
 // function: for loading(fetching) all phone from server.
 const loadingAllPhones = async () => {
+  // hiding spinner after fetching data
+  loader.classList.add("hidden");
+
   // fetching data from server
   const response = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=iphone`
   );
   const data = await response.json();
-  displayPhone(data);
+  const actualData = data.data;
+  displayPhone(actualData);
 };
 
 // function: for displaying phone as card on website
 const displayPhone = (phones) => {
-  console.log(phones);
+  // looping through the data to show
+  phones.forEach((phone) => {
+    console.log(phone);
+  });
 };
 
 // event: function for trigger a event when clicking on search button
 const searchingPhone = () => {
-  console.log("searching phone...");
+  // display loading spinner before data fetch
+  loader.classList.remove("hidden");
+
+  console.log(loader, "searching phone...");
 
   // for unknown delay time
   setTimeout(() => {
